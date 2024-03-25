267186620a6f8914ba6eb2ca95a5f17153ea6209
sreehari-zaigo <138750760+sreehari-zaigo@users.noreply.github.com>
2024-03-25 15:15:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,1 @@
-#!/usr/bin/env bash
 
-# Install dependencies
-apt-get update
-apt-get install -y wget gnupg
-
-# Add the Google Chrome PPA
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-
-# Install Google Chrome
-apt-get update
-apt-get install -y google-chrome-stable
-
-# Install Puppeteer
-npm install puppeteer
-
-# Build your application
-# Add any additional build steps specific to your application here
